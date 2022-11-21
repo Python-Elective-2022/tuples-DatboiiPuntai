a364cc2fb6a32cf92a3b8b4abb13708eed01dd68
Pantai <pantaisuyasri@gmail.com>
2022-11-21 11:10:21 +0700

    ggez

--- tuples_Pan_1102.py
+++ tuples_Pan_1102.py
@@ -8,9 +8,13 @@ def skip_tuples(t):
     # string slicing thingy
     return t[::2]
 
+
 def main():
     tuple1 = ('I', 'am', 'a', 'test', 'tuple')
+    tuple2 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     print(skip_tuples(tuple1))
+    print(skip_tuples(tuple2))
+
 
 if __name__ == '__main__':
     main()
